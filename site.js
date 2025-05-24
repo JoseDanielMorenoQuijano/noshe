7eda689f533b29a93c4219d65f022d200cb857b0
JoseDanielMorenoQuijano <myjed2127@gmail.com>
2025-05-24 16:43:59 -0600

    Add files via upload

---
+++ site.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    var button = document.querySelector('.gg-menu-oreos');
+    button.classList.toggle('gg-close');
+    var menu = document.getElementsByClassName('js-menu');
+    menu[0].classList.toggle("active");
+}
+
+function fixPhotoOnScroll () {
+    var photo = document.getElementById("photo-profile");
+    this.scrollY > 50 ? photo.style.marginTop = 0  : photo.style.marginTop = '7rem';
+}
+
+window.addEventListener("scroll", fixPhotoOnScroll , false);
